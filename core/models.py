dfcbc4aece613486aaffea9d925586880f070a3c
Malav <malavdesai2013@gmail.com>
2020-07-07 19:09:40 -0400

    Creating order summary in cart

--- core/models.py
+++ core/models.py
@@ -28,7 +28,7 @@
 # from django.db.models.signals import post_save
 from django.conf import settings
 from django.db import models
-# from django.db.models import Sum
+from django.db.models import Sum
 from django.shortcuts import reverse
 # from django_countries.fields import CountryField
 
@@ -100,19 +100,19 @@ class OrderItem(models.Model):
     def __str__(self):
         return f"{self.quantity} of {self.item.title}"
 
-    # def get_total_item_price(self):
-    #     return self.quantity * self.item.price
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
 
-    # def get_total_discount_item_price(self):
-    #     return self.quantity * self.item.discount_price
+    def get_total_discount_item_price(self):
+        return self.quantity * self.item.discount_price
 
-    # def get_amount_saved(self):
-    #     return self.get_total_item_price() - self.get_total_discount_item_price()
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_total_discount_item_price()
 
-    # def get_final_price(self):
-    #     if self.item.discount_price:
-    #         return self.get_total_discount_item_price()
-    #     return self.get_total_item_price()
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
 
 
 class Order(models.Model):
@@ -150,13 +150,13 @@ class Order(models.Model):
     def __str__(self):
         return self.user.username
 
-#     def get_total(self):
-#         total = 0
-#         for order_item in self.items.all():
-#             total += order_item.get_final_price()
-#         if self.coupon:
-#             total -= self.coupon.amount
-#         return total
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        # if self.coupon:
+        #     total -= self.coupon.amount
+        return total
 
 
 # class Address(models.Model):
