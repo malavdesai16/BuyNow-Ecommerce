e4b824aa9eda26147d0b1c5ac0deab97f37805ca
Jeet Patel <pateljeet777@gmail..com>
2020-07-06 21:06:35 -0400

    added slug and discount feature

--- core/models.py
+++ core/models.py
@@ -29,7 +29,7 @@
 from django.conf import settings
 from django.db import models
 # from django.db.models import Sum
-# from django.shortcuts import reverse
+from django.shortcuts import reverse
 # from django_countries.fields import CountryField
 
 
@@ -64,20 +64,20 @@ LABEL_CHOICES = (
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
-    # discount_price = models.FloatField(blank=True, null=True)
+    discount_price = models.FloatField(blank=True, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
-    # slug = models.SlugField()
-    # description = models.TextField()
+    slug = models.SlugField()
+    description = models.TextField()
     # image = models.ImageField()
 
     def __str__(self):
         return self.title
 
-    # def get_absolute_url(self):
-    #     return reverse("core:product", kwargs={
-    #         'slug': self.slug
-    #     })
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={
+            'slug': self.slug
+        })
 
     # def get_add_to_cart_url(self):
     #     return reverse("core:add-to-cart", kwargs={
@@ -92,16 +92,14 @@ class Item(models.Model):
 
 class OrderItem(models.Model):
     # user = models.ForeignKey(settings.AUTH_USER_MODEL,
-    #  on_delete=models.CASCADE)
+    #                          on_delete=models.CASCADE)
     # ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     # quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return self.title
-
-    # def __str__(self):
-    #     return f"{self.quantity} of {self.item.title}"
+        # return f"{self.quantity} of {self.item.title}"
 
     # def get_total_item_price(self):
     #     return self.quantity * self.item.price
