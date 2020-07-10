eafbc0b9299b76c04abb2f45b7c00a2a82bce04a
Malav <malavdesai2013@gmail.com>
2020-07-09 21:03:07 -0400

    Default Addresses(Billing & Shipping)

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Item, OrderItem, Order, Payment, Coupon, Refund
+from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address
 
 
 def make_refund_accepted(modeladmin, request, queryset):
@@ -16,12 +16,14 @@ class OrderAdmin(admin.ModelAdmin):
                     'received',
                     'refund_requested',
                     'refund_granted', 'billing_address',
+                    'shipping_address',
                     'payment',
                     'coupon']
 
     list_display_links = [
         'user',
         'billing_address',
+        'shipping_address',
         'payment',
         'coupon'
     ]
@@ -40,9 +42,24 @@ class OrderAdmin(admin.ModelAdmin):
     actions = [make_refund_accepted]
 
 
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'street_address',
+        'apartment_address',
+        'country',
+        'zip',
+        'address_type',
+        'default'
+    ]
+    list_filter = ['default', 'address_type', 'country']
+    search_fields = ['user', 'street_address', 'apartment_address', 'zip']
+
+
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
 admin.site.register(Coupon)
 admin.site.register(Refund)
+admin.site.register(Address, AddressAdmin)
